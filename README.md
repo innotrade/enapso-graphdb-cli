ff40e86b639d961fcbff521212fd94d28cdc9483
Alexander Schulze <aschulze@innotrade.com>
2019-02-26 12:47:51 +0100

    Submitting first version that is able to export a repository to file.

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # enapso-graphdb-cli
-Enapso GraphDB Command Line Interface
+Enapso Ontotext GraphDB 8.x Command Line Interface (CLI) for JavaScript
+
+## Commands
+
+export  Exports a repository or context from GraphDB to a file
+
+## Parameters
+--repository   -r
+--context      -c   context to be used for the command, of not passed usually the entire repository is used
+--username     -u
+--password     -p
+--dburl        -d
+--baseuri      -b
+--verbose      -v
+--targetfile   -t   file name for exports, if not given output ist send to the console
+--format       -f   please refer to the below format specification
+
+# Examples
+
+enapsoGDB export \
+    --baseuri http://localhost:7200 \
+    --repository Test \
+    --context http://ont.enapso.com/test \
+    --targetfile backups/backup.ttl \
+    --username Test \
+    --password Test \
+    --format text/turtle
+
+## File Types and Data Formats
+
+The following file types are supported:
+
+name: "JSON",
+type: "application/rdf+json",
+extension: ".json"
+
+name: "JSON-LD",
+type: "application/ld+json",
+extension: ".jsonld"
+
+name: "RDF-XML",
+type: "application/rdf+xml",
+extension: ".rdf"
+
+name: "N3",
+type: "text/rdf+n3",
+extension: ".n3"
+
+name: "N-Triples",
+type: "text/plain",
+extension: ".nt"
+
+name: "N-Quads",
+type: "text/x-nquads",
+extension: ".nq"
+
+name: "Turtle",
+type: "text/turtle",
+extension: ".ttl"
+
+name: "TriX",
+type: "application/trix",
+extension: ".trix"
+
+name: "TriG",
+type: "application/x-trig",
+extension: ".trig"
+
+name: "Binary RDF",
+type: "application/x-binary-rdf",
+extension: ".brf"
---
+++ enapsoGDB.js
@@ -0,0 +1,77 @@
+'use strict';
+// Innotrade Enapso GraphDB Command Line Interface (CLI)
+// (C) Copyright 2019 Innotrade GmbH, Herzogenrath, NRW, Germany
+
+// for details regarding the options see:
+// https://github.com/75lb/command-line-args/blob/master/doc/API.md
+// https://github.com/75lb/command-line-args/blob/master/doc/option-definition.md
+
+const commandLineArgs = require('command-line-args');
+const EnapsoGraphDBClient = require('enapso-graphdb-client');
+const EnapsoGraphDBAdmin = require('enapso-graphdb-admin');
+
+// the default prefixes for all SPARQL queries
+const GRAPHDB_DEFAULT_PREFIXES = [
+    EnapsoGraphDBClient.PREFIX_OWL,
+    EnapsoGraphDBClient.PREFIX_RDF,
+    EnapsoGraphDBClient.PREFIX_RDFS
+];
+
+const EnapsoGraphDBCLI = {
+
+    mOptionDefinitions: [
+        { name: "command", defaultOption: true },
+        { name: "repository", alias: 'r', type: String },
+        { name: "context", alias: 'c', type: String },
+        { name: "username", alias: 'u', type: String },
+        { name: "password", alias: 'p', type: String },
+        { name: "dburl", alias: 'd', type: String },
+        { name: "baseuri", alias: 'b', type: String },
+        { name: "verbose", alias: 'v', type: Boolean },
+        { name: "targetfile", alias: 't', type: String },
+        { name: "format", alias: 'f', type: String }
+    ],
+
+    export: async function (aOptions) {
+        var lRes = await this.endpoint.downloadToFile({
+            repository: aOptions.repository,
+            format: aOptions.format,
+            context: aOptions.context,
+            filename: aOptions.targetfile,
+            username: aOptions.username,
+            password: aOptions.password
+        });
+        console.log("Export file has been created.");
+    },
+
+    exec: async function () {
+        console.log("Enapso Ontotext GraphDB Command Line Interface");
+        console.log("(C) 2019 Innotrade GmbH, Herzogenrath, NRW, Germany");
+
+        let lOptions = commandLineArgs(this.mOptionDefinitions);
+        // console.log(JSON.stringify(lOptions, null, 2));
+
+        this.endpoint = new EnapsoGraphDBClient.Endpoint({
+            baseURL: lOptions.baseuri,
+            repository: lOptions.repository,
+            prefixes: GRAPHDB_DEFAULT_PREFIXES
+        });
+        this.authentication = await this.endpoint.login(
+            lOptions.username,
+            lOptions.password
+        );
+        if (!this.authentication.success) {
+            console.log("Login failed");
+            process.exit(-1);
+        }
+
+        if ('export' === lOptions.command) {
+            this.export(lOptions);
+        } else {
+            console.log("No valid command passed");
+        }
+    }
+
+}
+
+EnapsoGraphDBCLI.exec();
