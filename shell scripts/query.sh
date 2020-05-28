e4f03e1308d50a9d17331c0637914d4ac355c53c
Muhammad Yasir <yasir76892@gmail.com>
2020-05-28 18:12:43 +0500

    adding user name and password

--- shell scripts/query.sh
+++ shell scripts/query.sh
@@ -3,5 +3,5 @@ cd ..
 REPORT_FILE=Report/EnapsoTestReport.txt
 echo "Removing Previous Report File..."
 rm $REPORT_FILE
-enapsogdb query --dburl "http://localhost:7200" --repository "Test" --queryfile "queries/all.sparql" --prefixfile "queries/prefixes.prf" --targetfile "results/countries.csv">> $REPORT_FILE
+enapsogdb query --dburl "http://localhost:7200" --repository "Test" --username "Test" --password "Test" --queryfile "queries/all.sparql" --prefixfile "queries/prefixes.prf" --targetfile "results/countries.csv">> $REPORT_FILE
 echo "Enapso Test Suite Done"
