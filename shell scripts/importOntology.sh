7156d1e9e0dd38677e2c5f6aa138598b246cbb02
Muhammad Yasir <yasir76892@gmail.com>
2020-06-08 18:34:39 +0500

    minor fixes

--- shell scripts/importOntology.sh
+++ shell scripts/importOntology.sh
@@ -3,5 +3,5 @@ cd ..
 REPORT_FILE=Report/EnapsoTestReport.txt
 echo "Removing Previous Report File..."
 rm $REPORT_FILE
-enapsogdb import --dburl "http://localhost:7200" --repository "Test" --context "http://ont.enapso.com/dotnetpro" --baseiri "http://ont.enapso.com/dotnetpro#" --sourcefile "imports/dotnetpro_demo_ontology_2.owl" --username "admin" --password "root" --format "application/rdf+xml">> $REPORT_FILE
+enapsogdb import --dburl "http://localhost:7200" --repository "Test" --context "http://ont.enapso.com/repo" --baseiri "http://ont.enapso.com/repo#" --sourcefile "imports/EnapsoOntologyRepository.owl" --username "admin" --password "root" --format "application/rdf+xml">> $REPORT_FILE
 echo "Enapso Test Suite Done"
