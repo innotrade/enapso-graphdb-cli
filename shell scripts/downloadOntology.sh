c2dd4435b4ec4af1c21cac177265f6b50f8de89f
myasir92 <yasir76892@gmail.com>
2023-03-24 14:43:49 +0500

    adding new images

--- shell scripts/downloadOntology.sh
+++ shell scripts/downloadOntology.sh
@@ -3,5 +3,5 @@ cd ..
 REPORT_FILE=Report/EnapsoTestReport.txt
 echo "Removing Previous Report File..."
 rm $REPORT_FILE
-enapsogdb export --dburl "http://localhost:7200" --repository "Test" --context "http://ont.enapso.com/test" --targetfile "exports/export.ttl"  --username "admin" --password "root" --format "text/turtle">> $REPORT_FILE 
+enapsogdb export --dburl "http://localhost:5820" --repository "Test" --context "http://ont.enapso.com/test" --targetfile "exports/export.ttl"  --username "admin" --password "root" --format "text/turtle" --triplestore "stardog">> $REPORT_FILE 
 echo "ENAPSO Test Suite Done"
